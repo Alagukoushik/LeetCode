269574120c229207d32095ce1c4852f6230487c6
Alagukoushik <164029608+Alagukoushik@users.noreply.github.com>
2024-08-15 20:37:11 +0530

    Time: 0 ms (100%), Space: 42.4 MB (93.45%) - LeetHub

---
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int c=0;
+        for(int a:nums){
+            if(a<k){
+                c++;
+            }
+        }
+        return c;
+    }
+}
