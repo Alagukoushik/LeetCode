99e7063f510e8d5e404d2174708a92b20bb1c85c
Alagukoushik <164029608+Alagukoushik@users.noreply.github.com>
2024-08-18 22:22:56 +0530

    Time: 0 ms (100%), Space: 40.3 MB (39.64%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        String s="";
-        while(n>0){
-            s+=n%2;
-            n/=2;
-        }
-        System.out.print(s);  
         return false; 
     }
 }
